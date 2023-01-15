8d4da6d070237fe48688bb437c1dbf7f2863a812
GanaviM <85246503+GanaviM@users.noreply.github.com>
2023-01-15 22:28:55 +0530

    Add files via upload

---
+++ Airways_FlyAway/src/main/java/models/Password.java
@@ -0,0 +1,20 @@
+package models;
+/* Author Ganavi*/
+
+public class Password {
+	
+	private static String pwd;
+	
+	public Password() {
+		pwd = "admin";
+	}
+
+	public static String getPwd() {
+		return pwd;
+	}
+
+	public static void setPwd(String pwd) {
+		Password.pwd = pwd;
+	}
+
+}
